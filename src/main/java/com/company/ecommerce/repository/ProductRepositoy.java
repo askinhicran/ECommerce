6d5dd56455806b5c3ed5c6d9ffdedd61ad31fc1e
askinhicran <askinhicranozan@hotmail.com>
2020-03-27 11:07:56 +0300

    ProductRepo is edited for cleanCode.

--- src/main/java/com/company/ecommerce/repository/ProductRepositoy.java
+++ src/main/java/com/company/ecommerce/repository/ProductRepositoy.java
@@ -1,12 +1,8 @@
 package com.company.ecommerce.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
